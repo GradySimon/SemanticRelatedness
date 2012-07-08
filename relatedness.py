6f3767f3d7731eb92468cdae3d2496bcae12aca5
Grady Simon <grady@gradysimon.com>
2012-07-08 02:25:41 -0400

    Fleshed out the relatedness calculation method. The get_decaying_subgraph function it relies on is currently untested

--- relatedness.py
+++ relatedness.py
@@ -9,8 +9,7 @@ class Semantic_Network:
         self.graph.add_node(entity)
 
     def connect(self, first_entity, second_entity, strength):
-        edge = (first_entity, second_entity, {'capacity':strength})
-        self.graph.add_edge(*edge)
+        self.graph.add_edge(first_entity, second_entity, capacity=strength)
 
     def remove_entity(self, entity):
         self.graph.remove_node(entity)
@@ -18,5 +17,29 @@ class Semantic_Network:
     def remove_connection(self, first_entity, second_entity):
         self.graph.remove_edge(first_entity, second_entity)
 
-    # def calc_relatedness(self, first_entity, second_entity):
-        
+    def calc_relatedness(self, first_entity, second_entity):
+        subgraph = get_decaying_subgraph(self.graph, [first_entity, second_entity])
+        # calculate max flow from first_entity to second_entity        
+        return max_flow(subgraph, first_entity, second_entity)
+
+def get_decaying_subgraph(graph, start_nodes, max_hops=3, decay_rate=0.5):
+    subgraph = nx.Graph()
+    subgraph.add_node(start_node)
+    seen_node_set = set()
+    hop_node_set = set(start_nodes)
+    next_hop_set = set()
+    decay_multiplier = 1 - decay_rate
+    for hop in range(max_hops):
+        decay_multiplier **= hop 
+        for node in hop_node_set if node not in seen_node_set:
+            for neighbor in node.neighbors()
+                if subgraph.has_edge(node, neighbor):
+                    continue
+                next_hop_set.add(neighbor)
+                decayed_capacity = decay_multiplier * graph[node][neighbor]['capacity'] 
+                subgraph.add_edge(node, neighbor, capacity=decayed_capacity) 
+            seen_node_set.add(node)
+        hop_node_set = next_hop_set
+        next_hop_set.clear()
+    return subgraph
+                
